27856c083b74794948dc565a131f8fd9a0bb7f20
Yaslim2 <yaslimsoares15@gmail.com>
2023-01-09 18:23:25 -0300

    ✨ add prisma integration and some routes for test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { PrismaService } from './prisma.service';
+import { Controller, Get, Post } from '@nestjs/common';
+import { randomUUID } from 'crypto';
 
-@Controller()
+@Controller('notifications')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly prismaService: PrismaService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async list() {
+    return await this.prismaService.notification.findMany();
+  }
+
+  @Post()
+  async create() {
+    return await this.prismaService.notification.create({
+      data: {
+        id: randomUUID(),
+        content: 'Você tem uma nova solicitação de amizade!',
+        category: 'social',
+        recipientId: randomUUID(),
+      },
+    });
   }
 }
